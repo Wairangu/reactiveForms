90f0a146a2ee13d16a63477eb034c2f2119e6721
Isaac Wairangu <wairangui@gmail.com>
2019-05-19 12:04:11 +0300

    Added Form groups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,14 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female', 'Other'];
   signupForm: FormGroup;
-
+  
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      userData: new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+
       'gender': new FormControl('male')
     });
 
